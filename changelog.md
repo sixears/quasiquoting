ec10c0412480a5bfe4ff2bd4db6c93c3c9661ea9
Mr. Fluffy <git@sixearsm>
2022-11-17 19:33:29 +0000

    upgrade to callPackage-based versions

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+1.0.1.13 2022-11-17
+===================
+- upgrade to callPackage-based versions
+
 1.0.1.12 2022-11-13
 ===================
 - fix fixed-package-name typo in flake-build-utils
--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
     nixpkgs.url          = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url      = "github:sixears/flake-build-utils/r1.0.0.10";
+    build-utils.url      = "github:sixears/flake-build-utils/r1.0.0.11";
 
-    monaderror-io.url    = "github:sixears/monaderror-io/r1.2.5.9";
-    more-unicode.url     = "github:sixears/more-unicode/r0.0.17.7";
-    parsec-plus-base.url = "github:sixears/parsec-plus-base/r1.0.5.10";
-    tfmt.url             = "github:sixears/tfmt/r0.2.7.10";
+    monaderror-io.url    = "github:sixears/monaderror-io/r1.2.5.10";
+    more-unicode.url     = "github:sixears/more-unicode/r0.0.17.8";
+    parsec-plus-base.url = "github:sixears/parsec-plus-base/r1.0.5.11";
+    tfmt.url             = "github:sixears/tfmt/r0.2.7.11";
   };
 
   outputs = { self, nixpkgs, build-utils
