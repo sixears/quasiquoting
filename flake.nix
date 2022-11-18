7eead537bc81c4f58fa7992d22fe923a70a447b0
Mr. Fluffy <git@sixearsm>
2022-11-18 21:17:52 +0000

    index->1.0.1.17;base0t->0.0.1.10;number->1.1.2.11;base0->0.0.4.8;more-unicode->0.0.17.9;flake-build-utils->1.0.0.12

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url          = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url      = "github:sixears/flake-build-utils/r1.0.0.12";
 
-    monaderror-io.url    = "github:sixears/monaderror-io/r1.2.5.13";
+    monaderror-io.url    = github:sixears/monaderror-io/r1.2.5.14;
     more-unicode.url     = "github:sixears/more-unicode/r0.0.17.9";
-    parsec-plus-base.url = "github:sixears/parsec-plus-base/r1.0.5.15";
-    tfmt.url             = github:sixears/tfmt/r0.2.7.16;
+    parsec-plus-base.url = github:sixears/parsec-plus-base/r1.0.5.16;
+    tfmt.url             = github:sixears/tfmt/r0.2.7.18;
   };
 
   outputs = { self, nixpkgs, build-utils
