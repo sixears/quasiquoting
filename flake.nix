924579daf3f81c1267ee9d94a735e07ddb926760
Mr. Fluffy <git@sixearsm>
2022-11-19 06:10:38 +0000

    flake-build-utils->1.0.0.13

--- flake.nix
+++ flake.nix
@@ -2,21 +2,32 @@
   description = "Containers that may not be empty, by construction";
 
   inputs = {
-    nixpkgs.url          = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url      = "github:sixears/flake-build-utils/r1.0.0.12";
+    nixpkgs.url          = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+    build-utils.url      = github:sixears/flake-build-utils/r1.0.0.13;
 
-    monaderror-io.url    = github:sixears/monaderror-io/r1.2.5.14;
-    more-unicode.url     = "github:sixears/more-unicode/r0.0.17.9";
-    parsec-plus-base.url = github:sixears/parsec-plus-base/r1.0.5.16;
-    tfmt.url             = github:sixears/tfmt/r0.2.7.18;
+    monaderror-io.url    = github:sixears/monaderror-io/r1.2.5.15;
+    more-unicode.url     = github:sixears/more-unicode/r0.0.17.10;
+    parsec-plus-base.url = github:sixears/parsec-plus-base/r1.0.5.17;
+    tfmt.url             = github:sixears/tfmt/r0.2.7.19;
   };
 
   outputs = { self, nixpkgs, build-utils
             , monaderror-io, more-unicode, parsec-plus-base, tfmt }:
     build-utils.lib.hOutputs self nixpkgs "quasiquoting" {
-      deps = {
-        inherit monaderror-io more-unicode parsec-plus-base tfmt;
-      };
       ghc = p: p.ghc8107; # for tfmt
+      callPackage = { mkDerivation, lib, mapPkg, system
+                    , base, base-unicode-symbols, data-default, lens
+                    , template-haskell, text
+                    }:
+        mkDerivation {
+          pname = "quasiquoting";
+          version = "1.0.1.23";
+          src = ./.;
+          libraryHaskellDepends = [
+            base base-unicode-symbols data-default lens template-haskell text
+          ] ++ mapPkg [ monaderror-io more-unicode parsec-plus-base tfmt ] ;
+          description = "manage info.yaml";
+          license = lib.licenses.mit;
+        };
     };
 }
