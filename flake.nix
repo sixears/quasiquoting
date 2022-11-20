b2c06847deb7e45b8656f61f3274bc157420d933
Mr. Fluffy <git@sixearsm>
2022-11-20 07:18:35 +0000

    has-callstack->1.0.1.19

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url          = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url      = github:sixears/flake-build-utils/r1.0.0.13;
 
-    monaderror-io.url    = github:sixears/monaderror-io/r1.2.5.19;
+    monaderror-io.url    = github:sixears/monaderror-io/r1.2.5.20;
     more-unicode.url     = github:sixears/more-unicode/r0.0.17.12;
-    parsec-plus-base.url = github:sixears/parsec-plus-base/r1.0.5.22;
-    tfmt.url             = github:sixears/tfmt/r0.2.7.24;
+    parsec-plus-base.url = github:sixears/parsec-plus-base/r1.0.5.23;
+    tfmt.url             = github:sixears/tfmt/r0.2.7.25;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -21,7 +21,7 @@
                     }:
         mkDerivation {
           pname = "quasiquoting";
-          version = "1.0.1.31";
+          version = "1.0.1.32";
           src = ./.;
           libraryHaskellDepends = [
             base base-unicode-symbols data-default lens template-haskell text
